9a7723c71c3b065c6ccccffdb78d76e0a2c9a779
cuijia1247 <cuijia1247@163.com>
2025-08-25 17:50:02 +0800

    Initial commit: Add Streamlit tutorial files

---
+++ tut1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("Hello World")
+st.write(123123)
+st.write({"a":1, "b":2})
+st.write([1, 2, "apple", "banana", "!"])
---
+++ tut2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+print("start run")
+pressed1 = st.button("button 1")
+print("pressed1:", pressed1)
+
+pressed2 = st.button("button 2")
+print("pressed2:", pressed2)
+
+pressed3 = st.button("button 3", icon=":material/home:")fatkun
